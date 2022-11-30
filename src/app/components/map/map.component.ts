93e0da40ae88378e68ab00eb2ca19582d8cd8b88
Keegan Osler <keegan.osler@vergeag.com>
2022-11-30 16:38:46 -0700

    new daynight layer every 30 seconds

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -11,8 +11,10 @@ import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
 import { fromLonLat } from 'ol/proj';
 import { XYZ } from 'ol/source';
+import VectorSource from 'ol/source/Vector';
 import Fill from 'ol/style/Fill';
 import Style from 'ol/style/Style';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-map',
@@ -31,24 +33,46 @@ export class MapComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
+    this.createInitialMap();
+    this.generateDayNightLayer();
+    interval(30000).subscribe(() => {
+      this.generateDayNightLayer();
+    });
+  }
+
+  createInitialMap() {
     this.map = new Map({
       target: this.mapRef?.nativeElement,
       layers: [this.baselayer],
       view: new View({
-        center: fromLonLat([-101.320426, 42.041532]),
-        zoom: 4,
+        center: fromLonLat([0, 0]),
+        zoom: 1,
       }),
     });
+  }
 
-    this.map.addLayer(
-      new VectorLayer({
-        source: new DayNight({}),
-        style: new Style({
-          fill: new Fill({
-            color: [0, 0, 50, 0.5],
-          }),
+  generateDayNightLayer() {
+    let dayNightLayer: VectorLayer<VectorSource> = new VectorLayer({
+      source: new DayNight({
+        time: new Date(),
+      }),
+      style: new Style({
+        fill: new Fill({
+          color: [0, 0, 50, 0.5],
         }),
-      })
-    );
+      }),
+      properties: {
+        isDayNightLayer: true,
+      },
+    });
+    this.map
+      ?.getLayers()
+      .getArray()
+      .forEach((layer) => {
+        if (layer.get('isDayNightLayer')) {
+          this.map?.removeLayer(layer);
+        }
+      });
+    this.map?.addLayer(dayNightLayer);
   }
 }
